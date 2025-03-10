c0bdda66cbd50d2d73c074c036a2494d73b2cb5f
Ruwaiz Razak <ruwaizrazak@gmail.com>
2025-03-10 20:00:44 +0100

    updated maturity

--- src/utils/maturityIcons.ts
+++ src/utils/maturityIcons.ts
@@ -2,9 +2,9 @@ export const getMaturityIcon = (
   maturity: 'seed' | 'plant' | 'tree' = 'seed'
 ): string => {
   const iconMap = {
-    seed: '/icons/seed.gif',
+    seed: '/icons/seed.svg',
     plant: '/icons/plant.gif',
-    tree: '/icons/tree.gif',
+    tree: '/icons/tree.svg',
   };
 
   return iconMap[maturity] || iconMap.seed;
